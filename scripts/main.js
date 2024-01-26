ef27438392331e3f040d4d30b88509cb73400238
stephenmoll <stephenmollandco@mail.com>
2024-01-26 16:51:07 +0200

    Close button and menu tweaks

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     const mobileMenuIcon = document.getElementById('mobileMenuIcon');
     const nav = document.querySelector('nav');
+    const closeMenuButton = document.getElementById('closeMenu');
 
     mobileMenuIcon.addEventListener('click', function () {
         nav.classList.toggle('open');
+        closeMenuButton.style.display = 'block';
+    });
+
+    closeMenuButton.addEventListener('click', function () {
+        nav.classList.remove('open');
+        closeMenuButton.style.display = 'none';
     });
 });
