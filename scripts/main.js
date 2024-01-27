3f8a50c154087cd71a49fd4cc7d6527a4b9775fc
stephenmoll <stephenmollandco@mail.com>
2024-01-27 14:23:41 +0200

    map

--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,17 @@ document.addEventListener('DOMContentLoaded', function () {
     const mobileMenuIcon = document.getElementById('mobileMenuIcon');
     const nav = document.querySelector('nav');
     const closeMenuButton = document.getElementById('closeMenu');
+    const googleMap = document.getElementById('googleMap')
 
     mobileMenuIcon.addEventListener('click', function () {
         nav.classList.toggle('open');
         closeMenuButton.style.display = 'block';
+        googleMap.style.display = 'block'; 
     });
 
     closeMenuButton.addEventListener('click', function () {
         nav.classList.remove('open');
         closeMenuButton.style.display = 'none';
+        googleMap.style.display = 'none'; 
     });
 });
